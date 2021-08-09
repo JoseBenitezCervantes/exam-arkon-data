6f05b01f508f7aae03368ad10b3baa854fd21b8a
iMac Jose Benitez <jose.benitez@elektra.com.mx>
2021-08-09 11:13:10 -0500

    Fecha aleatoria

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -141,15 +141,18 @@ const setRamdonTask = async (req, res = response) => {
     "necesitar",
     "querer",
   ];
-  const time = Date.now();
-  const now = new Date(time);
-  for (let index = 0; index < 50; index++) {
+
+  for (let index = 0; index < 10; index++) {
     const randomNum = (min, max) => {
       return Math.floor(Math.random() * (max - min + 1) + min);
     };
+    const time = Date.now();
+    const now = new Date(time);
     const randomMinutes = randomNum(30, 60);
     const randomSec = randomNum(1, 60);
     const randomName = randomNum(0, arrNameTask.length - 1);
+    const randomDay = randomNum(1, 7);
+    now.setDate(now.getDate() + randomDay);
     arrTask[index] = {
       name: arrNameTask[randomName],
       description: "Description test",
